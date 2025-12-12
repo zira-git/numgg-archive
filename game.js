881e8c25dbaf3fd5d1e60f32d1740b485396ce81
zira <ziraproton@proton.me>
2025-12-11 20:53:40 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,52 +1,30 @@
-bestEver = 0;
-bestEver = localStorage.getItem("bestEver") || 0
+@@ -1,4 +1,4 @@
+
 bestScore = 0;
 streak = 0;
-
-
 function start() {
-
-
-
     RightRandomNumber = Math.floor(Math.random() * 100)
-    secondRandomNumber = Math.floor(Math.random() * 100)
-    firstRandomNumber = Math.floor(Math.random() * 100)
-@@ -20,7 +22,9 @@ function start() {
-        document.getElementById('btn3').innerHTML = RightRandomNumber;
-        document.getElementById('btn1').innerHTML = firstRandomNumber;
-        document.getElementById('btn2').innerHTML = secondRandomNumber;
-    } 
-    }
-console.log(RightRandomNumber)
-    document.getElementById("be").innerHTML = localStorage.getItem("bestEver");
-}
-
-
-@@ -53,7 +57,7 @@ function appear() {
-}
-
-function hintTimer() {
-    setTimeout(appear, 1000 * 10)
-    setTimeout(appear, 1000 * 30)
-}
-
+@@ -23,10 +23,16 @@ function start() {
 function inc() {
-@@ -66,6 +70,12 @@ function inc() {
-    } else if(document.getElementById('be').innerHTML == 0 || document.getElementById('be').innerHTML < streak || document.getElementById('be').innerHTML < bestScore) {
-        bestEver = bestScore;
+    streak++;
+    document.getElementById("xp").innerHTML = streak;
+    if(document.getElementById('highScore').innerHTML == 0 || document.getElementById('highScore').innerHTML < streak ) {
+        bestScore = streak;
+        document.getElementById('highScore').innerHTML = bestScore;
+        console.log(streak)
     }
-    
-    if (bestScore > bestEver ) {
-        bestEver = bestScore;
-        localStorage.setItem("bestEver", bestEver.toString())
-    }
-
 }
 
 function lose() {
-@@ -103,4 +113,4 @@ function f3() {
-    }
+    document.getElementById("xp").innerHTML = "0";
+    streak = 0;
+    document.getElementById("xp").innerHTML = streak;
 }
 
-
-console.log(RightRandomNumber)
+function f1() {
+@@ -57,4 +63,5 @@ function f3() {
+                start(); lose()
+        window.alert("YOU DIED!!!");
+    }
+}
+}
