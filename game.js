1c155f6ee41d6126b493113e48556f2f6bbf7b8d
zira <ziraproton@proton.me>
2025-12-13 15:06:15 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,26 @@
-@@ -1,4 +1,4 @@
-
+@@ -1,60 +1,67 @@
 bestScore = 0;
 streak = 0;
 function start() {
     RightRandomNumber = Math.floor(Math.random() * 100)
-@@ -23,10 +23,16 @@ function start() {
+    secondRandomNumber = Math.floor(Math.random() * 100)
+    firstRandomNumber = Math.floor(Math.random() * 100)
+    let select = Math.floor(Math.random() * 3)
+    if (select == 0) {
+        document.getElementById('btn1').innerHTML = RightRandomNumber;
+        document.getElementById('btn2').innerHTML = firstRandomNumber;
+        document.getElementById('btn3').innerHTML = secondRandomNumber;
+    } else if (select == 1) {
+        document.getElementById('btn2').innerHTML = RightRandomNumber;
+        document.getElementById('btn1').innerHTML = firstRandomNumber;
+        document.getElementById('btn3').innerHTML = secondRandomNumber;
+    } else if (select == 2) {
+        document.getElementById('btn3').innerHTML = RightRandomNumber;
+        document.getElementById('btn1').innerHTML = firstRandomNumber;
+        document.getElementById('btn2').innerHTML = secondRandomNumber;
+    } 
+}
+
 function inc() {
     streak++;
     document.getElementById("xp").innerHTML = streak;
@@ -16,15 +32,36 @@ function inc() {
 }
 
 function lose() {
-    document.getElementById("xp").innerHTML = "0";
     streak = 0;
     document.getElementById("xp").innerHTML = streak;
 }
 
 function f1() {
-@@ -57,4 +63,5 @@ function f3() {
+    if (document.getElementById('btn1').innerHTML == RightRandomNumber) {
+        window.alert("YOU WERE RIGHT!!!")
+        inc(); start(); 
+    } else if (document.getElementById('btn1').innerHTML == firstRandomNumber || document.getElementById('btn1').innerHTML == secondRandomNumber) {
+        window.alert("YOU DIED!!!");
+        start(); lose()
+    }
+}
+
+function f2() {
+    if (document.getElementById('btn2').innerHTML == RightRandomNumber) {
+        window.alert("YOU WERE RIGHT!!!")
+        inc(); start(); 
+    } else if (document.getElementById('btn2').innerHTML == firstRandomNumber || document.getElementById('btn2').innerHTML == secondRandomNumber) {
+        window.alert("YOU DIED!!!");
+        start(); lose()
+    }
+}
+
+function f3() {
+    if (document.getElementById('btn3').innerHTML == RightRandomNumber) {
+        window.alert("YOU WERE RIGHT!!!")
+        inc(); start(); 
+    } else if (document.getElementById('btn3').innerHTML == firstRandomNumber || document.getElementById('btn3').innerHTML == secondRandomNumber) {
                 start(); lose()
         window.alert("YOU DIED!!!");
     }
-}
 }
