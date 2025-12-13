4eb468867990402a40dbb86b5b77ee19e572b1e0
zira <ziraproton@proton.me>
2025-12-13 15:29:44 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,56 @@
-@@ -43,49 +43,49 @@
+@@ -1,67 +1,106 @@
+bestEver = 0;
+bestScore = 0;
+streak = 0;
+function start() {
+    RightRandomNumber = Math.floor(Math.random() * 100)
+    secondRandomNumber = Math.floor(Math.random() * 100)
+    firstRandomNumber = Math.floor(Math.random() * 100)
+    let select = Math.floor(Math.random() * 3)
+    if (select == 0) {
+        document.getElementById('btn1').innerHTML = RightRandomNumber;
+        document.getElementById('btn2').innerHTML = firstRandomNumber;
+        document.getElementById('btn3').innerHTML = secondRandomNumber;
+    } else if (select == 1) {
+        document.getElementById('btn2').innerHTML = RightRandomNumber;
+        document.getElementById('btn1').innerHTML = firstRandomNumber;
+        document.getElementById('btn3').innerHTML = secondRandomNumber;
+    } else if (select == 2) {
+        document.getElementById('btn3').innerHTML = RightRandomNumber;
+        document.getElementById('btn1').innerHTML = firstRandomNumber;
+        document.getElementById('btn2').innerHTML = secondRandomNumber;
+    } 
 }
 
+function hint() {
+    if(document.getElementById('btn1').innerHTML == RightRandomNumber) {
+         document.getElementById('btn1').style.backgroundColor = "red;";        
+    } else if(document.getElementById('btn2').innerHTML == RightRandomNumber) {
+        document.getElementById('btn2').style.backgroundColor = 'red';
+    } else if(document.getElementById('btn3').innerHTML == RightRandomNumber) {
+        document.getElementById('btn3').style.backgroundColor = 'red';
+    } else {
+        throw new Error(`Error@ ${document.title}`);
+        
+    }
+        document.getElementById('h').style.display = "none"
+        setTimeout(function(){    if(document.getElementById('btn1').innerHTML == RightRandomNumber) {
+         document.getElementById('btn1').style.backgroundColor = "greenyellow";        
+    } else if(document.getElementById('btn2').innerHTML == RightRandomNumber) {
+        document.getElementById('btn2').style.backgroundColor = 'greenyellow';
+    } else if(document.getElementById('btn3').innerHTML == RightRandomNumber) {
+        document.getElementById('btn3').style.backgroundColor = 'greenyellow';
+    } else {
+        throw new Error(`Error@ ${document.title}`);
+        
+    }},1000 * 2)
+}
+function appear() {
+    document.getElementById('h').style.display = 'block'
+}
 function hintTimer() {
-    setTimeout(appear, 1000 * 0.5)
-    setTimeout(appear, 1000 * 30)
+    setTimeout(appear, 1000 * 10)
 }
-
 function inc() {
     streak++;
     document.getElementById("xp").innerHTML = streak;
@@ -16,12 +61,6 @@ function inc() {
     } else if(document.getElementById('be').innerHTML == 0 || document.getElementById('be').innerHTML < streak || document.getElementById('be').innerHTML < bestScore) {
         bestEver = bestScore;
     }
-
-    if (bestScore > bestEver ) {
-        bestEver = bestScore;
-        localStorage.setItem("bestEver", bestEver.toString())
-    }
-
 }
 
 function lose() {
@@ -48,3 +87,15 @@ function f2() {
         start(); lose()
     }
 }
+
+function f3() {
+    if (document.getElementById('btn3').innerHTML == RightRandomNumber) {
+        window.alert("YOU WERE RIGHT!!!")
+        inc(); start(); 
+    } else if (document.getElementById('btn3').innerHTML == firstRandomNumber || document.getElementById('btn3').innerHTML == secondRandomNumber) {
+                start(); lose()
+        window.alert("YOU DIED!!!");
+    }
+}
+
+
